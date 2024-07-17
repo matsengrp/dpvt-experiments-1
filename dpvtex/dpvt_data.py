5e9694b3e6f273e0cf872491c3f50829c8897057
lenacoll <lena@lenacoll.de>
2024-07-17 13:24:29 -0700

    small fix in train/val split

--- dpvtex/dpvt_data.py
+++ dpvtex/dpvt_data.py
@@ -96,14 +96,14 @@ def train_val_data_of_nicknames(data_name):
     # use number of bad edges to stratify dataset
     n_bad_edges = np.array([sum(label) for label in labels])
 
+    # TODO: Replace with just taking last 20% as validation set!
     train_data, val_data, train_labels, val_labels, train_mask, val_mask = (
         train_test_split(
             trees,
             labels,
             masks,
-            n_bad_edges,
-            train_size=train_size,
-            test_size=val_size,
+            train_size=0.8,
+            test_size=0.2,
             stratify=n_bad_edges,
             random_state=42,
         )

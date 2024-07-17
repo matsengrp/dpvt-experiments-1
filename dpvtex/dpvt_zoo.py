1a9da0d4b8e5ba6efaf7241ce0c4812bb01d78b6
lenacoll <lena@lenacoll.de>
2024-07-17 13:25:30 -0700

    simplify loading checkpoints + set num_threads = 1

--- dpvtex/dpvt_zoo.py
+++ dpvtex/dpvt_zoo.py
@@ -1,7 +1,3 @@
-import pandas as pd
-import os
-from pathlib import Path
-
 from dpvt import models
 from dpvt.wrapper import Wrap, HyperWrap
 from dpvtex.dpvt_data import (
@@ -9,6 +5,9 @@ from dpvtex.dpvt_data import (
     train_val_data_of_nicknames,
 )
 
+import torch
+torch.set_num_threads(1)
+torch.set_default_dtype(torch.float64)  # Set default to float64 for higher precision
 
 def get_model(model_name):
     if model_name == "TraverseNN":
@@ -25,24 +24,26 @@ def get_model(model_name):
 def trained_model_str(model_name, data_name):
     return f"{model_name}-{data_name}"
 
+def tested_model_str(model_name, train_data_name, test_data_name):
+    return f"{model_name}-{train_data_name}-ON-{test_data_name}"
 
 def trained_model_path(model_name, data_name):
-    return "trained_models/" + trained_model_str(model_name, data_name)
+    return f"trained_models/{trained_model_str(model_name, data_name)}"
 
+def tested_model_path(model_name, train_data_name, test_data_name):
+    return f"tested_models/{tested_model_str(model_name, train_data_name, test_data_name)}"
 
 def best_model_params_path(model_name, data_name):
     return f"hyper_checkpoints/{trained_model_str(model_name, data_name)}"
 
 
-def train_model(model_name, data_name, final_checkpoint=None, **wrap_kwargs):
+def train_model(model_name, data_name, train_checkpoint=None, **wrap_kwargs):
     """
     Creates a model in class `model_name` and trains it on data `data_name`.
     """
     # set final and test checkpoint strings
-    if final_checkpoint is None:
-        final_checkpoint = trained_model_path(model_name, data_name) + ".ckpt"
-    # if test_checkpoint is None:
-    #     test_checkpoint = trained_model_path(model_name, data_name) + "_test.ckpt"
+    if train_checkpoint is None:
+        train_checkpoint = trained_model_path(model_name, data_name) + ".ckpt"
     # hyperparameters (only used if no hyperparameter testing done)
     default_params = {
         "learning_rate": 0.01,
@@ -51,29 +52,38 @@ def train_model(model_name, data_name, final_checkpoint=None, **wrap_kwargs):
     }
     # Update default parameters with any provided keyword arguments
     wrap_params = {**default_params, **wrap_kwargs}
-    train_data, val_data, test_data = train_val_data_of_nicknames(data_name)
+    train_data, val_data = train_val_data_of_nicknames(data_name)
     model = get_model(model_name)
     model_str = trained_model_str(model_name, data_name)
     wrap = Wrap(
-        train_data, val_data, test_data, model, log_path=model_str, **wrap_params
+        train_data,
+        val_data,
+        test_data=None,
+        model=model,
+        log_path=model_str,
+        **wrap_params,
     )
-    wrap.train(final_checkpoint)
-    # wrap.test(test_checkpoint)
+    wrap.train(train_checkpoint)
     return model
 
 
-def continue_train_model(
-    model_name, data_name, final_checkpoint=None, test_checkpoint=None, **wrap_kwargs
-):
+def continue_train_model(model_name, data_name, train_checkpoint=None, **wrap_kwargs):
     """
     Loads a model in class `model_name` that was previously trained on `data_name`, and
     continue training it.
     """
     # set final and test checkpoint strings
-    if final_checkpoint is None:
-        final_checkpoint = trained_model_path(model_name, data_name) + ".ckpt"
-    if test_checkpoint is None:
-        test_checkpoint = trained_model_path(model_name, data_name) + "_test.ckpt"
+    if train_checkpoint is None:
+        train_checkpoint = trained_model_path(model_name, data_name) + ".ckpt"
+    # load trained model
+    try:
+        model = get_model(model_name).load_from_checkpoint(train_checkpoint)
+    except FileNotFoundError as e:
+        raise ValueError(
+            f"Model {model_name} trained on data {data_name} does not have saved checkpoint."
+        ) from e
+
+    model_str = trained_model_str(model_name, data_name)    
     # hyperparameters (only used if no hyperparameter testing done)
     default_params = {
         "learning_rate": 0.01,
@@ -82,34 +92,22 @@ def continue_train_model(
     }
     # Update default parameters with any provided keyword arguments
     wrap_params = {**default_params, **wrap_kwargs}
-    # load trained model
-    script_directory = Path(__file__)
-    path = script_directory.parent.parent / "train"
-    path = path / (trained_model_path(model_name, data_name) + ".ckpt")
-    try:
-        model = get_model(model_name).load_from_checkpoint(path)
-    except FileNotFoundError as e:
-        raise ValueError(
-            f"Model {model_name} trained on data {data_name} does not have saved checkpoint."
-        ) from e
-    model_str = trained_model_str(model_name, data_name)
     # load dataset
-    train_data, val_data, test_data = train_val_data_of_nicknames(data_name)
+    train_data, val_data = train_val_data_of_nicknames(data_name)
     wrap = Wrap(
         train_data=train_data,
         val_data=val_data,
-        test_data=test_data,
+        test_data=None,
         model=model,
         log_path=model_str,
         **wrap_params,
     )
-    wrap.train(final_checkpoint)
-    results = wrap.test(test_checkpoint)
-    return results
+    wrap.train(train_checkpoint)
+    return model
 
 
 def optimize_hyperparameters(model_name, data_name, best_model_hparams_filepath):
-    train_data, val_data, _ = train_val_data_of_nicknames(data_name)
+    train_data, val_data, = train_val_data_of_nicknames(data_name)
     model = get_model(model_name)
     model_str = trained_model_str(model_name, data_name)
     hyper_wrap = HyperWrap(
@@ -119,32 +117,18 @@ def optimize_hyperparameters(model_name, data_name, best_model_hparams_filepath)
     return model
 
 
-def test_trained_model(
-    model_name, trained_data_name, test_data_name, test_checkpoint=None, **wrap_kwargs
+def test_model(
+    trained_model_name, train_data_name, trained_model_ckpt, test_data_name, test_checkpoint, **wrap_kwargs
 ):
     """
-    Loads a trained model, specified by `model_name` and `trained_data_name`, and
-    tests the model on the dataset `test_data_name`
+    Loads a trained model, specified by `model_name` and `trained_data_name`, loads
+    it from checkpoint `trained_model_ckpt` and tests it on `test_data_name` dataset
+    and saves trained model to checkpoint `test_checkpoint`.
     """
-    # create string for logging
-    model_str = trained_model_str(model_name, trained_data_name)
-    model_str = model_str + "-ON-" + test_data_name
-    # set test checkpoint string
-    if test_checkpoint is None:
-        test_checkpoint = (
-            "trained_models/" + model_str + "_test.ckpt"
-        )
-    # hyperparameters (only used if no hyperparameter testing done)
-    # default_params = {
-    #     "learning_rate": 0.01,
-    #     "batch_size": 1024,
-    #     "epochs": 2,
-    # }
     # Update default parameters with any provided keyword arguments
     wrap_params = {**wrap_kwargs}
-    # load trained model
-    path = "../train/" + trained_model_path(model_name, trained_data_name) + ".ckpt"
-    model = get_model(model_name).load_from_checkpoint(path)
+    model = get_model(trained_model_name).load_from_checkpoint(trained_model_ckpt)
+    model_str = tested_model_str(trained_model_name, train_data_name, test_data_name)
     # load dataset
     test_data = data_of_nicknames(test_data_name)
     test_wrap = Wrap(
@@ -161,4 +145,3 @@ def test_trained_model(
 
     test_auroc = result[0]["test_auroc"]
     return test_auroc
-    # write result to csv file?

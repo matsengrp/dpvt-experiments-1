e361c8ae842146e01d43653010a677b9bd487f2c
Harry Richman <dhrichman@gmail.com>
2024-07-25 10:09:37 -0700

    Add function to validate model (#10)
    * wip - add function to validate trained model
    
    * add function to validate saved trained model on new dataset
    
    * separate arguments trained_data_name and val_data_name
    
    * add function to continue training model
    
    * add default checkpoint strings for train_model()
    
    * remove unused code
    
    * comment out unused code
    
    * add new function to load data without train-val-test split
    
    * replace validate_model with test_trained_model
    
    * remove debugging code
    
    * add string method for TreeDataset, add checkpoint string to test_trained_model
    
    * modify checkpoint string for test_trained_model
    
    * modify train_val_data_of_nicknames() to just return train and val splits, no test split
    
    * add setup file
    
    * small fix in train/val split
    
    * simplify loading checkpoints + set num_threads = 1
    
    * separate snakemake rule for testing
    
    * add test data sets
    
    * increase number of epochs
    
    * match testing and training data in snakefile
    
    * del comment -- we need to shuffle to split data
    
    * update README to include separation of test and train data
    
    ---------
    
    Co-authored-by: lenacoll <lena@lenacoll.de>

--- dpvtex/dpvt_data.py
+++ dpvtex/dpvt_data.py
@@ -13,7 +13,10 @@ script_directory = Path(__file__).resolve().parent
 dataset_dict = {
     "FourLeafFourSite": script_directory.parent / "data/4leaf4site.p",
     "FourLeaf": script_directory.parent / "data/4leaf.p",
+    "FourLeafFourSiteTest": script_directory.parent / "data/4leaf4site_test.p",
+    "FourLeafTest": script_directory.parent / "data/4leaf_test.p",
     "TenLeaf": script_directory.parent / "data/10leaf_perfect.p",
+    "TenLeafTest": script_directory.parent / "data/10leaf_test.p",
     "ThirtyLeaf": script_directory.parent / "data/30leaf_perfect.p",
 }
 
@@ -30,6 +33,43 @@ class TreeDataset(Dataset):
     def __getitem__(self, idx):
         return self.data[idx], self.labels[idx], self.mask[idx]
 
+    def __str__(self):
+        n_leaves = 1 + len(self.data[0])  # number of leaves in `self.data[0]`
+        n_unmasked = sum(self.mask[0])
+        return (
+            f"TreeDataset\n"
+            f"Number of samples: {len(self.data)}\n"
+            f"Leaves per tree: {n_leaves}\n"
+            f"Unmasked edges per tree: {n_unmasked}"
+        )
+
+
+def data_of_nicknames(data_name):
+    """
+    Takes a dataset nickname string, which is a key in `dataset_dict`, and returns the
+    corresponding data as a `TreeDataset` object.
+    """
+    file_path = dataset_dict[data_name]
+    file_path = os.path.realpath(file_path)
+    with open(file_path, "rb") as f:
+        data_dict = pickle.load(f)
+
+    labels = list(data_dict.values())
+    trees = list(data_dict.keys())
+
+    masks = []
+    for tree in trees:
+        # mask leaves, root (which is leaf) and root (which contains data for edge
+        # leading to root leaf)
+        mask_list = [
+            not (node.is_leaf() or node.is_root() or node.up.is_root())
+            for node in tree.traverse("preorder")
+        ]
+        masks.append(mask_list)
+
+    tree_data = TreeDataset(trees, labels, masks)
+    return tree_data
+
 
 def train_val_data_of_nicknames(data_name):
     file_path = dataset_dict[data_name]
@@ -37,10 +77,9 @@ def train_val_data_of_nicknames(data_name):
     with open(file_path, "rb") as f:
         data_dict = pickle.load(f)
 
-    # split into 60% training, 20% validation, 20% testing
-    train_size = int(0.6 * len(data_dict))
-    val_size = int(0.2 * len(data_dict))
-    test_size = len(data_dict) - train_size - val_size
+    # split into 80% training, 20% validation
+    train_size = int(0.8 * len(data_dict))
+    val_size = len(data_dict) - train_size
 
     # Split into balanced training, validation, and test data using sklearn
     labels = list(data_dict.values())
@@ -56,33 +95,21 @@ def train_val_data_of_nicknames(data_name):
         ]
         masks.append(mask_list)
 
-    # labels_array = np.array(labels)
+    # use number of bad edges to stratify dataset
     n_bad_edges = np.array([sum(label) for label in labels])
-
-    (
-        train_val_data,
-        test_data,
-        train_val_labels,
-        test_labels,
-        train_val_mask,
-        test_mask,
-        sum_train_val,
-        _,
-    ) = train_test_split(
-        trees, labels, masks, n_bad_edges, test_size=test_size, stratify=n_bad_edges
-    )
 
     train_data, val_data, train_labels, val_labels, train_mask, val_mask = (
         train_test_split(
-            train_val_data,
-            train_val_labels,
-            train_val_mask,
-            test_size=val_size / (train_size + val_size),
-            stratify=sum_train_val,
+            trees,
+            labels,
+            masks,
+            train_size=0.8,
+            test_size=0.2,
+            stratify=n_bad_edges,
+            random_state=42,
         )
     )
 
     train_data = TreeDataset(train_data, train_labels, train_mask)
-    test_data = TreeDataset(test_data, test_labels, test_mask)
     val_data = TreeDataset(val_data, val_labels, val_mask)
-    return train_data, val_data, test_data
+    return train_data, val_data

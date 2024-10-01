6501b5ea258abee196f89d6fed5e26f9920b817f
lenacoll <lena@lenacoll.de>
2024-10-01 16:53:40 -0700

    add simulated dataset nickname

--- dpvtex/dpvt_data.py
+++ dpvtex/dpvt_data.py
@@ -35,9 +35,10 @@ dataset_dict = {
     / "data/harrington-small_2024-06-10_train.p",
     "HarringtonTinyTest": script_directory.parent / "data/harrington_tiny_test.p",
     "HarringtonBelow50": script_directory.parent / "data/harrington-small_0_to_50_taxa_subset.p",
-    "HarringtonShortSequences": script_directory.parent / "data/harrington-small_0_to_30_sites_subset.p"
+    "HarringtonShortSequences": script_directory.parent / "data/harrington-small_0_to_30_sites_subset.p",
+    "Alisim10leaf_10sites_50alignments_train": script_directory.parent / "data/larch_alisim_10_seq_10_site_50_alignments_2024-10-01_train.p",
+    "Alisim10leaf_10sites_50alignments_test": script_directory.parent / "data/larch_alisim_10_seq_10_site_50_alignments_2024-10-01_test.p",
 }
-
 
 def data_of_nicknames(data_name, device):
     """

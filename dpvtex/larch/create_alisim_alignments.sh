355b8153f7e78b4e5706bc589170b2c237844eab
lenacoll <lena@lenacoll.de>
2024-10-30 16:34:38 -0700

    add script to simulate alignments w/ alisim

---
+++ dpvtex/larch/create_alisim_alignments.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Parameters
+num_alignments=500
+num_sequences=20
+alignment_length=100
+base_directory="../data/alisim_alignment_${num_sequences}_seq_${alignment_length}_sites_${num_alignments}_algnmnts"
+
+# Create base directory for alignments
+mkdir -p "$base_directory"
+
+for i in $(seq 1 $num_alignments); do
+    # Create directory for each alignment
+    alignment_dir="$base_directory/alignment_$i"
+    mkdir -p "$alignment_dir"
+
+    # Create the alignment and save it as a FASTA file
+    iqtree --alisim $alignment_dir/alignment_$i -t RANDOM{yh/$num_sequences} --length $alignment_length --out-format fasta -redo
+
+    mv $alignment_dir/alignment_$i.fa $alignment_dir/alignment_$i.fasta
+
+    echo "Created $alignment_dir/alignment_$i.fasta"
+done

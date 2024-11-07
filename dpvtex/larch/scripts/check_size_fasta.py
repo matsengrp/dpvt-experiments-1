ea9527b19d834784fac4b8e5ec135c8a19938fa1
lenacoll <lena@lenacoll.de>
2024-11-07 15:13:34 -0800

    add description and clean up check_size_fasta.py

--- dpvtex/larch/scripts/check_size_fasta.py
+++ dpvtex/larch/scripts/check_size_fasta.py
@@ -3,14 +3,14 @@ import sys
 from shutil import rmtree
 
 def main():
+    # Check whether the input fasta file has at least 5 sites
+    # If it does, create a flag, so the generate_dpvt_input.snakefile is only run for data where this flag exists
     fasta_file = sys.argv[1]
     output_flag = sys.argv[2]
-    empty = True
     for record in SeqIO.parse(fasta_file, "fasta"):
         if len(record.seq.strip()) > 5:
-            empty = False
             with open(output_flag, 'w') as flag:
-                flag.write("EMPTY" if empty else "NOT_EMPTY")
+                flag.write("NOT_EMPTY")
         else:
             break
 

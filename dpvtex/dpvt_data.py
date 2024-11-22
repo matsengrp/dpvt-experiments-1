02d3ce9baed11e2bec46880cc0563183f8001753
Lena Collienne <lena@lenacoll.de>
2024-11-22 04:34:18 -0800

    Training on GPU (#13)
    * mv Datasets class to dpvt + update epochs and n_trials
    
    * masking moved to dpvt
    
    * some minor things
    
    * enable training on GPU
    
    * choose device in configfile
    
    * del masking from dpvt_data + only use traversal dataset when not using cpu
    
    * add benchmarking to training
    
    * fix typo
    
    * hyperparam: n_trials default to 10 for now
    
    * improve splitting of train, test, and validation set
    
    * add explanations for training and config file arguments to README
    
    * only use tree datastructure if requested, even on CPU
    
    * profiling argument
    
    * update gitignore
    
    * formatting + random seeds
    
    * fixing some minor things
    
    * specify tree dataset for training, too
    
    * del redundancies
    
    * formatting
    
    * fix hyperparameter loading + add memory profiling for training
    
    * add accum_grad_batches
    
    * update hparams, formatting + proper hparams search
    
    * name input arguments for test_model()
    
    * update default hparams + num_trials=100
    
    * rm defaults channel from conda env
    
    * correctly load hparams for testing
    
    * default hparams are defined in model definition
    
    * explanation for perfect phylogenies data generation in separate README
    
    * update README
    
    * cleanup
    
    * mv TransformerEncoderTraversal to separate branch (wip)
    
    * rm memory profiling
    
    * small clenup

--- dpvtex/dpvt_data.py
+++ dpvtex/dpvt_data.py
@@ -1,8 +1,6 @@
 import pickle
 from sklearn.model_selection import train_test_split
-from torch.utils.data import (
-    Dataset,
-)
+from dpvt.wrapper import TreeDataset, TraversalDataset
 from pathlib import Path
 import os
 import pandas as pd
@@ -17,30 +15,8 @@ data_dir = dataset_dict.pop("data_dir")
 
 dataset_dict = {key: data_dir + "/" + dataset_dict[key] for key in dataset_dict}
 
-class TreeDataset(Dataset):
-    def __init__(self, data, labels, mask):
-        self.data = data
-        self.labels = labels
-        self.mask = mask
 
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        return self.data[idx], self.labels[idx], self.mask[idx]
-
-    def __str__(self):
-        n_leaves = 1 + len(self.data[0])  # number of leaves in `self.data[0]`
-        n_unmasked = sum(self.mask[0])
-        return (
-            f"TreeDataset\n"
-            f"Number of samples: {len(self.data)}\n"
-            f"Leaves per tree: {n_leaves}\n"
-            f"Unmasked edges per tree: {n_unmasked}"
-        )
-
-
-def data_of_nicknames(data_name):
+def data_of_nicknames(data_name, device):
     """
     Takes a dataset nickname string, which is a key in `dataset_dict`, and returns the
     corresponding data as a `TreeDataset` object.
@@ -53,21 +29,13 @@ def data_of_nicknames(data_name):
     labels = list(data_dict.values())
     trees = list(data_dict.keys())
 
-    masks = []
-    for tree in trees:
-        # mask leaves, root (which is leaf) and root (which contains data for edge
-        # leading to root leaf)
-        mask_list = [
-            not (node.is_leaf() or node.is_root() or node.up.is_root())
-            for node in tree.traverse("preorder")
-        ]
-        masks.append(mask_list)
-
-    tree_data = TreeDataset(trees, labels, masks)
+    if device == "cpu-tree-dataset":
+        tree_data = TreeDataset(trees, labels)
+    else:
+        tree_data = TraversalDataset(trees, labels, device)
     return tree_data
 
-
-def train_val_data_of_nicknames(data_name):
+def train_val_data_of_nicknames(data_name, device):
     file_path = dataset_dict[data_name]
     file_path = os.path.realpath(file_path)
     with open(file_path, "rb") as f:
@@ -77,16 +45,6 @@ def train_val_data_of_nicknames(data_name):
     labels = list(data_dict.values())
     trees = list(data_dict.keys())
 
-    masks = []
-    for tree in trees:
-        # mask leaves, root (which is leaf) and root (which contains data for edge
-        # leading to root leaf)
-        mask_list = [
-            not (node.is_leaf() or node.is_root() or node.up.is_root())
-            for node in tree.traverse("preorder")
-        ]
-        masks.append(mask_list)
-
     sum_of_ones = [sum(label) for label in labels]
     counter = Counter(sum_of_ones)
 
@@ -95,17 +53,22 @@ def train_val_data_of_nicknames(data_name):
         sum_of_ones, q=min(len(counter), 4), labels=False, duplicates="drop"
     )
 
-    train_data, val_data, train_labels, val_labels, train_mask, val_mask = (
-        train_test_split(
-            trees,
-            labels,
-            masks,
-            train_size=0.8,
-            test_size=0.2,
-            stratify=categories,
-        )
+    train_data, val_data, train_labels, val_labels = train_test_split(
+        trees,
+        labels,
+        train_size=0.8,
+        test_size=0.2,
+        stratify=categories,
+        random_state=42,
     )
 
-    train_data = TreeDataset(train_data, train_labels, train_mask)
-    val_data = TreeDataset(val_data, val_labels, val_mask)
+    if device == "cpu-tree-dataset":
+        # no need to convert to traversal data structure, as this would add
+        # one more traversal, hence increase runtime
+        train_data = TreeDataset(train_data, train_labels)
+        val_data = TreeDataset(val_data, val_labels)
+    else:
+        train_data = TraversalDataset(train_data, train_labels, device)
+        val_data = TraversalDataset(val_data, val_labels, device)
+
     return train_data, val_data
--- dpvtex/dpvt_zoo.py
+++ dpvtex/dpvt_zoo.py
@@ -4,10 +4,15 @@ from dpvtex.dpvt_data import (
     data_of_nicknames,
     train_val_data_of_nicknames,
 )
-
+import json
 import torch
+
 torch.set_num_threads(1)
+from pytorch_lightning import seed_everything
+
+# seed_everything(42, workers=True)
 torch.set_default_dtype(torch.float64)  # Set default to float64 for higher precision
+
 
 def get_model(model_name):
     if model_name == "TraverseNN":
@@ -16,43 +21,52 @@ def get_model(model_name):
         model = models.TraverseMaxPooling
     elif model_name == "TraverseAvgPooling":
         model = models.TraverseAvgPooling
-    elif model_name == "TransformerEncoderTraversal":
-        model = models.TransformerEncoderTraversal
     return model
 
 
 def trained_model_str(model_name, data_name):
     return f"{model_name}-{data_name}"
 
+
 def tested_model_str(model_name, train_data_name, test_data_name):
     return f"{model_name}-{train_data_name}-ON-{test_data_name}"
+
 
 def trained_model_path(model_name, data_name):
     return f"trained_models/{trained_model_str(model_name, data_name)}"
 
+
 def tested_model_path(model_name, train_data_name, test_data_name):
-    return f"tested_models/{tested_model_str(model_name, train_data_name, test_data_name)}"
+    return (
+        f"tested_models/{tested_model_str(model_name, train_data_name, test_data_name)}"
+    )
+
 
 def best_model_params_path(model_name, data_name):
     return f"hyper_checkpoints/{trained_model_str(model_name, data_name)}"
 
 
-def train_model(model_name, data_name, train_checkpoint=None, **wrap_kwargs):
+def train_model(
+    model_name,
+    data_name,
+    train_checkpoint,
+    device,
+    hyperparameter_path,
+    accum_grad_batches=1,
+    feature_length=32,
+    dim_mlp_layers=32,
+    profiling=False,
+    **wrap_kwargs,
+):
     """
     Creates a model in class `model_name` and trains it on data `data_name`.
     """
     # set final and test checkpoint strings
     if train_checkpoint is None:
         train_checkpoint = trained_model_path(model_name, data_name) + ".ckpt"
-    # hyperparameters (only used if no hyperparameter testing done)
-    default_params = {
-        "learning_rate": 0.01,
-        "batch_size": 1024,
-        "epochs": 200,
-    }
     # Update default parameters with any provided keyword arguments
-    wrap_params = {**default_params, **wrap_kwargs}
-    train_data, val_data = train_val_data_of_nicknames(data_name)
+    wrap_params = {**wrap_kwargs}
+    train_data, val_data = train_val_data_of_nicknames(data_name, device)
     model = get_model(model_name)
     model_str = trained_model_str(model_name, data_name)
     wrap = Wrap(
@@ -61,13 +75,30 @@ def train_model(model_name, data_name, train_checkpoint=None, **wrap_kwargs):
         test_data=None,
         model=model,
         log_path=model_str,
+        profiling=profiling,
+        device=device,
+        accum_grad_batches=accum_grad_batches,
+        feature_length=feature_length,
+        dim_mlp_layers=dim_mlp_layers,
+        hyperparameter_path=hyperparameter_path,
         **wrap_params,
     )
     wrap.train(train_checkpoint)
     return model
 
 
-def continue_train_model(model_name, data_name, train_checkpoint=None, **wrap_kwargs):
+def continue_train_model(
+    model_name,
+    data_name,
+    device,
+    hyperparameter_path,
+    epochs=200,
+    accum_grad_batches=1,
+    feature_length=32,
+    dim_mlp_layers=32,
+    train_checkpoint=None,
+    **wrap_kwargs,
+):
     """
     Loads a model in class `model_name` that was previously trained on `data_name`, and
     continue training it.
@@ -83,42 +114,58 @@ def continue_train_model(model_name, data_name, train_checkpoint=None, **wrap_kw
             f"Model {model_name} trained on data {data_name} does not have saved checkpoint."
         ) from e
 
-    model_str = trained_model_str(model_name, data_name)    
-    # hyperparameters (only used if no hyperparameter testing done)
-    default_params = {
-        "learning_rate": 0.01,
-        "batch_size": 1024,
-        "epochs": 100,
-    }
+    model_str = trained_model_str(model_name, data_name)
     # Update default parameters with any provided keyword arguments
-    wrap_params = {**default_params, **wrap_kwargs}
+    wrap_params = {**wrap_kwargs}
     # load dataset
-    train_data, val_data = train_val_data_of_nicknames(data_name)
+    train_data, val_data = train_val_data_of_nicknames(data_name, device)
     wrap = Wrap(
         train_data=train_data,
         val_data=val_data,
         test_data=None,
         model=model,
         log_path=model_str,
+        device=device,
+        epochs=epochs,
+        accum_grad_batches=accum_grad_batches,
+        feature_length=feature_length,
+        dim_mlp_layers=dim_mlp_layers,
+        hyperparameter_path=hyperparameter_path,
         **wrap_params,
     )
     wrap.train(train_checkpoint)
     return model
 
 
-def optimize_hyperparameters(model_name, data_name, best_model_hparams_filepath):
-    train_data, val_data, = train_val_data_of_nicknames(data_name)
+def optimize_hyperparameters(
+    model_name, data_name, best_model_hparams_filepath, device, profiling=False
+):
+    train_data, val_data = train_val_data_of_nicknames(data_name, device)
     model = get_model(model_name)
     model_str = trained_model_str(model_name, data_name)
     hyper_wrap = HyperWrap(
-        model, train_data, val_data, model_str, n_trials=1
-    )  # n_trials chosen small for testing
+        model,
+        train_data,
+        val_data,
+        model_str,
+        profiling=profiling,
+        device=device,
+        n_trials=100,
+    )
     hyper_wrap.optuna_optimize(best_model_hparams_filepath)
     return model
 
 
 def test_model(
-    trained_model_name, train_data_name, trained_model_ckpt, test_data_name, test_checkpoint, **wrap_kwargs
+    trained_model_name,
+    train_data_name,
+    trained_model_ckpt,
+    test_data_name,
+    test_checkpoint,
+    device,
+    hyperparameter_path,
+    accum_grad_batches=1,
+    **wrap_kwargs,
 ):
     """
     Loads a trained model, specified by `model_name` and `trained_data_name`, loads
@@ -127,21 +174,24 @@ def test_model(
     """
     # Update default parameters with any provided keyword arguments
     wrap_params = {**wrap_kwargs}
-    model = get_model(trained_model_name).load_from_checkpoint(trained_model_ckpt)
+    model = get_model(trained_model_name)
+    with open(hyperparameter_path, "r") as f:
+        hparams = json.load(f)
+    model.load_from_checkpoint(trained_model_ckpt, learning_rate = hparams["learning_rate"], feature_length = hparams["feature_length"], dim_mlp_layers = hparams["dim_mlp_layers"])
     model_str = tested_model_str(trained_model_name, train_data_name, test_data_name)
     # load dataset
-    test_data = data_of_nicknames(test_data_name)
+    test_data = data_of_nicknames(test_data_name, device)
     test_wrap = Wrap(
         train_data=None,
         val_data=None,
         test_data=test_data,
         model=model,
         log_path=model_str,
+        device=device,
+        accum_grad_batches=accum_grad_batches,
+        hyperparameter_path=hyperparameter_path,
         **wrap_params,
     )
 
     # evaluate model
-    result = test_wrap.test(test_checkpoint)
-
-    test_auroc = result[0]["test_auroc"]
-    return test_auroc
+    test_wrap.test(trained_model_ckpt, test_checkpoint)

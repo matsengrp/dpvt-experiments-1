24d080dd5fda0c86d0967386ace21d921d14bdd8
David Rich <david.rich27@gmail.com>
2024-12-12 07:33:59 -0800

    fixes to custom callback and to plot notebook.

--- dpvtex/dpvt_zoo.py
+++ dpvtex/dpvt_zoo.py
@@ -7,11 +7,17 @@ from dpvtex.dpvt_data import (
 import json
 import torch
 
+from lightning.pytorch.callbacks import Callback
+
 torch.set_num_threads(1)
 from pytorch_lightning import seed_everything
 
 # seed_everything(42, workers=True)
 torch.set_default_dtype(torch.float64)  # Set default to float64 for higher precision
+
+from datetime import datetime
+
+todays_date = datetime.now().strftime("%Y-%m-%d")
 
 
 def get_model(model_name):
@@ -32,6 +38,12 @@ def tested_model_str(model_name, train_data_name, test_data_name):
     return f"{model_name}-{train_data_name}-ON-{test_data_name}"
 
 
+def model_str(model_name, train_data_name, test_data_name=None):
+    if test_data_name:
+        return tested_model_str(model_name, train_data_name, test_data_name)
+    return trained_model_str(model_name, train_data_name)
+
+
 def trained_model_path(model_name, data_name):
     return f"trained_models/{trained_model_str(model_name, data_name)}"
 
@@ -46,18 +58,12 @@ def best_model_params_path(model_name, data_name):
     return f"hyper_checkpoints/{trained_model_str(model_name, data_name)}"
 
 
-def lightning_log_path(device, model_name, data_name, date="TIMESTAMP"):
-    return f"lightning_logs/{device}_{date}/{trained_model_str(model_name, data_name)}"
+def csv_log_path(model_name, train_data_name, test_data_name=None, device=None, date=str(todays_date)):
+    path = f"result_csvs/{device}_{date}"
+    return f"{path}/{model_str(model_name, train_data_name, test_data_name)}.csv"
 
 
-def csv_log_path(model_name, train_data_name, test_data_name=None, device=None, date="TIMESTAMP"):
-    dir_path = f"csvs/{device}_{date}"
-    if test_data_name:
-        return f"{dir_path}/{tested_model_str(model_name, train_data_name, test_data_name)}"
-    return f"{dir_path}/{trained_model_str(model_name, train_data_name)}.csv"
-
-
-def generate_csv_log_paths(model_names, data_pairs, device=None, date="TIMESTAMP"):
+def generate_csv_log_paths(model_names, data_pairs, device=None, date=str(todays_date)):
     csv_log_paths = []
     for model_name in model_names:
         for train_data_name,test_data_name in data_pairs:
@@ -77,6 +83,7 @@ def train_model(
     feature_length=32,
     dim_mlp_layers=32,
     profiling=False,
+    timestamp=str(todays_date),
     **wrap_kwargs,
 ):
     """
@@ -102,6 +109,8 @@ def train_model(
         feature_length=feature_length,
         dim_mlp_layers=dim_mlp_layers,
         hyperparameter_path=hyperparameter_path,
+        added_callbacks=[TimerCallback()],
+        timestamp=timestamp,
         **wrap_params,
     )
     wrap.train(train_checkpoint)
@@ -177,6 +186,7 @@ def optimize_hyperparameters(
         profiling=profiling,
         device=device,
         n_trials=n_trials,
+        added_callbacks=[],
     )
     hyper_wrap.optuna_optimize(best_model_hparams_filepath)
     return model
@@ -222,40 +232,170 @@ def test_model(
     # evaluate model
     test_wrap.test(trained_model_ckpt, test_checkpoint)
 
+import os
+import tbparse
+import pandas as pd
+
+# get lightning logs from training and testing
+def get_lightning_log_path(model_name, train_data_name, device, timestamp, root_dir, test_data_name=None, version=None):
+    path = f'{root_dir}/lightning_logs/{device}_{timestamp}/{model_str(model_name, train_data_name, test_data_name)}'
+    path = append_version_to_path(path, version)
+    return path
+
+# get lightning logs from hyperparameter optimization
+def get_hyperparam_log_path(model_name, train_data_name, device, timestamp, root_dir, test_data_name=None, version=None):
+    path = f'{root_dir}/hyper_checkpoints/{model_str(model_name, train_data_name)}'
+    path = append_version_to_path(path, version)
+    return path
+
+# get json of hyperparameters
+def get_hyperparam_json_path(model_name, train_data_name, device, timestamp, root_dir, test_data_name=None, version=None):
+    path = f'{root_dir}/hyper_checkpoints/{model_str(model_name, train_data_name)}.json'
+    path = append_version_to_path(path, version)
+    return path
+
+# get dataframe from lightning log
+def get_df_from_log(log_path):
+    reader = tbparse.SummaryReader(log_path)
+    return reader.scalars
+
+# append version folder
+def append_version_to_path(path, version=None):
+    if version:
+        return f'{path}/{version}'
+    return path
+
+
 def aggregate_data_to_csv(
-    trained_model_name,
+    model_name,
     train_data_name,
-    trained_model_ckpt,
     test_data_name,
-    test_checkpoint,
     device,
+    timestamp,
+    n_hyperparameter_trials,
     hyperparameter_path,
-    lightning_log_path,
-    csv_outpath
+    trained_model_ckpt,
+    tested_model_ckpt,
+    csv_outpath,
 ):
   """
   Aggregate result data in a CSV entry.
   """
 
-  import numpy as np
-  import pandas as pd
-  import tbparse
-
-  print("aggregate data [BEGIN]")
-
+  # fetch hyperparams
   with open(hyperparameter_path) as f:
-      best_hyperparams = json.load(f)
-  print(f"best_hyperparams:\n{best_hyperparams}")
-  print(f"csv_outpath: {csv_outpath}")
+      opt_hyperparams = json.load(f)
+  # print(f"best_hyperparams:\n{opt_hyperparams}")
 
-  df = pd.DataFrame({
-    'model': [trained_model_name],
+  # fetch logs
+  root_dir = '.'
+  version = 'version_0'
+  hyperparam_json_path = get_hyperparam_json_path(model_name, train_data_name, device, timestamp, root_dir=root_dir, test_data_name=None, version=version)
+  hyperparam_log_path = get_hyperparam_log_path(model_name, train_data_name, device, timestamp, root_dir=root_dir, test_data_name=None, version=version)
+  train_log_path = get_lightning_log_path(model_name, train_data_name, device, timestamp, root_dir=root_dir, test_data_name=None, version=version)
+  test_log_path = get_lightning_log_path(model_name, train_data_name, device, timestamp, root_dir=root_dir, test_data_name=test_data_name, version=version)
+
+  log_dfs = {}
+  log_dfs['log_hyperparam'] = get_df_from_log(f'{hyperparam_log_path}')
+  log_dfs['log_train'] = get_df_from_log(f'{train_log_path}')
+  log_dfs['log_test'] = get_df_from_log(f'{test_log_path}')
+
+  # for key,df in log_dfs.items():
+  #   print('KEY:', key)
+  #   print(set(df.tag))
+  #   for tag in set(df.tag):
+  #       tag_df = df[df.tag == tag]
+  #       print(f'{tag}: {len(tag_df)}')
+  #       print(f'max_step: {max(set(tag_df.step))}')
+  #       print(f'max_value: {max(set(tag_df.value))}')
+
+  # fetch training stats
+  df = log_dfs['log_train']
+  train_walltime = df[df.tag == 'train_wall_time'].value.iloc[0]
+  train_epochs = df[df.tag == 'train_final_epoch'].value.iloc[0]
+  # train_steps = df[df.tag == 'train_final_step'].value.iloc[0]
+
+  # fetch testing stats
+  df = log_dfs['log_test']
+  test_auroc = df[df.tag == 'test_auroc'].value.iloc[0]
+  test_loss = df[df.tag == 'test_loss'].value.iloc[0]
+
+  df_row = pd.DataFrame({
+    # config settings
+    'model': [model_name],
     'train_data': [train_data_name],
     'test_data': [test_data_name],
-    'learning_rate': [best_hyperparams['learning_rate']],
-    'batch_size': [best_hyperparams['batch_size']],
-    'accum_grad_batches': [best_hyperparams['accum_grad_batches']]
+    'device': [device],
+    'timestamp': [timestamp],
+    'n_hyperparam_trials': [n_hyperparameter_trials],
+    # hyperparameters
+    'learning_rate': [opt_hyperparams['learning_rate']],
+    'batch_size': [opt_hyperparams['batch_size']],
+    'accum_grad_batches': [opt_hyperparams['accum_grad_batches']],
+    'max_epochs': [opt_hyperparams['epochs']],
+    'feature_length': [opt_hyperparams['feature_length']],
+    'dim_mlp_layers': [opt_hyperparams['dim_mlp_layers']],
+    # number of training steps, epochs
+    'train_steps': [-1],
+    'train_epochs': [train_epochs],
+    # test auroc
+    'test_auroc': [test_auroc],
+    'test_loss': [test_loss],
+    # runtime
+    'train_walltime': [train_walltime],
+    # paths
+    'hyperparam_json_path': [hyperparam_json_path],
+    'hyperparam_log_path': [hyperparam_log_path],
+    'train_log_path': [train_log_path],
+    'test_log_path': [test_log_path],
+    'trained_model_ckpt_path': [trained_model_ckpt],
+    'tested_model_ckpt_path': [tested_model_ckpt],
   })
 
-  print("aggregate data [END]")
+  # output single row to file
+  print(f"csv_outpath: {csv_outpath}")
+  df_row.to_csv(csv_outpath, index=False)
+
+  # append row to final file
+  csv_final_outpath = 'result_csvs/FINAL.csv'
+  print(f"csv_final_outpath: {csv_final_outpath}")
+  if os.path.exists(csv_final_outpath):
+      final_df = pd.read_csv(csv_final_outpath)
+      final_df = pd.concat([final_df, df_row], ignore_index=True)
+      final_df.to_csv(csv_final_outpath, index=False)
+  else:
+      df_row.to_csv(csv_final_outpath, index=False)
   return
+
+import time
+
+class TimerCallback(Callback):
+    """
+    Callback for logging hyperparameters, total_epochs, number_of_steps, auroc, runtimes
+    """
+    def __init__(self):
+        self.start_time = {}
+        self.total_steps = {}
+
+    def log_start(self, trainer, pl_module, prefix=""):
+        self.start_time[prefix] = time.time()
+        self.total_steps[prefix] = 0
+
+    def log_end(self, trainer, pl_module, prefix=""):
+        total_time = time.time() - self.start_time[prefix]
+        trainer.logger.log_metrics({f"{prefix}_wall_time": total_time})
+        trainer.logger.log_metrics({f"{prefix}_final_epoch": trainer.current_epoch})
+        stopped_early = trainer.current_epoch + 1 < trainer.max_epochs
+        trainer.logger.log_metrics({f"{prefix}_stopped_early": stopped_early})
+        trainer.logger.log_metrics({f"{prefix}_final_step": self.total_steps[prefix]})
+
+    def log_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx, prefix=""):
+        self.total_steps[prefix] += 1
+
+    # hooks
+
+    def on_train_start(self, trainer, pl_module):
+        self.log_start(trainer, pl_module, "train")
+
+    def on_train_end(self, trainer, pl_module):
+        self.log_end(trainer, pl_module, "train")
